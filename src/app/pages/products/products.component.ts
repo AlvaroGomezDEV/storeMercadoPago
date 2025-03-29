eaf37edc570b91f0e66e2c57222d5fb397053c50
Alvaro Gomez <alvaro@supermark.co>
2025-03-29 11:53:37 -0500

    products template 90%

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,13 +1,21 @@
-import { Component, inject } from "@angular/core";
-import { ProductsStore } from "../products.component.store";
-import { CommonModule } from "@angular/common";
+import { Component, inject } from '@angular/core';
+import { ProductsStore } from '../products.component.store';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
-  selector: "app-products",
-  imports: [CommonModule],
-  templateUrl: "./products.component.html",
-  styleUrl: "./products.component.css",
+  selector: 'app-products',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css',
 })
 export default class ProductsComponent {
   readonly store = inject(ProductsStore);
+
+  router = inject(Router);
+
+  goToProductDetail(productId: number) {
+    console.log('id: ', productId);
+    this.router.navigate(['/detail', productId]);
+  }
 }
